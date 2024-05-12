588a23fcfa5461f64fbaed3c5b519ad0a98ea415
Jeff Balagosa <jeff.balagosa@gmail.com>
2024-05-12 12:20:12 -0700

    Solve QuickSort implementation.

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,11 +1,20 @@
-function qs(arr: number[], lo: number, hi: number): void {}
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
+
+    const pivotIdx = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
+}
 
 function partition(arr: number[], lo: number, hi: number): number {
     const pivot = arr[hi];
 
     let idx = lo - 1;
 
-    for (let i = 0; i < hi; i++) {
+    for (let i = lo; i < hi; i++) {
         if (arr[i] <= pivot) {
             idx++;
             const tmp = arr[i];
@@ -21,4 +30,6 @@ function partition(arr: number[], lo: number, hi: number): number {
     return idx;
 }
 
-export default function quick_sort(arr: number[]): void {}
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+}
