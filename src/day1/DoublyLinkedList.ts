60ceaa084fbfeb37a6f9a89acca3760dc97b1ca9
jeff.balagosa <jeff.balagosa@.corp.vegas.com>
2024-05-30 15:41:11 -0700

    Complete remove function.

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -30,11 +30,11 @@ export default class DoublyLinkedList<T> {
     }
 
     insertAt(item: T, idx: number): void {
-        if (idx > this.length){
-            throw new Error("Index is ouside of list.")
+        if (idx > this.length) {
+            throw new Error("Index is ouside of list.");
         }
 
-        if ( idx === this.length) {
+        if (idx === this.length) {
             this.append(item);
             return;
         } else if (idx === 0) {
@@ -44,21 +44,20 @@ export default class DoublyLinkedList<T> {
 
         this.length++;
         let curr = this.head;
-        for (let i = 0; curr && i < idx; i++){
-            curr=curr.next;
+        for (let i = 0; curr && i < idx; i++) {
+            curr = curr.next;
         }
 
         curr = curr as Node<T>;
-        const node = {value: item} as Node<T>;
+        const node = { value: item } as Node<T>;
 
         node.next = curr;
         node.prev = curr.prev;
         curr.prev = node;
 
-        if (curr.prev){
+        if (curr.prev) {
             curr.prev.next = curr;
         }
-
     }
 
     append(item: T): void {
@@ -75,7 +74,38 @@ export default class DoublyLinkedList<T> {
         this.tail = node;
     }
 
-    remove(item: T): T | undefined {}
+    remove(item: T): T | undefined {
+        let curr = this.head;
+        for (let i = 0; curr && i < this.length; ++i) {
+            if (curr.value === item) {
+                break;
+            }
+            curr = curr.next;
+        }
+        if (!curr) {
+            return undefined;
+        }
+        this.length--;
+        if (this.length === 0) {
+            const out = this.head?.value;
+            this.head = this.tail = undefined;
+            return out;
+        }
+        if (curr.prev) {
+            curr.prev = curr.next;
+        }
+        if (curr.next) {
+            curr.next = curr.prev;
+        }
+        if (curr === this.head) {
+            this.head = curr.next;
+        }
+        if (curr === this.tail) {
+            this.tail = curr.prev;
+        }
+        curr.prev = curr.next = undefined;
+        return curr.value;
+    }
 
     get(idx: number): T | undefined {}
 
